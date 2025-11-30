2beee335f40df010885998fd9ddd0fb5e1bb3f48
yousemazhar <yousef.abdelmohsen@student.giu-uni.de>
2025-12-01 00:00:39 +0200

    2ay 7aga just to merge

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -4,25 +4,25 @@ import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-    // Enable CORS for clients that will send cookies
-    app.enableCors({
-        origin: true,
-        credentials: true,
-    });
+  // Enable CORS for clients that will send cookies
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
 
-    // Parse cookies for JWT strategy to read auth_token
-    app.use(cookieParser());
+  // Parse cookies for JWT strategy to read auth_token
+  app.use(cookieParser());
 
-    const config = new DocumentBuilder()
-        .setTitle('HR Platform API')
-        .setDescription('The HR Platform API description')
-        .setVersion('1.0')
-        .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+  const config = new DocumentBuilder()
+    .setTitle('HR Platform API')
+    .setDescription('The HR Platform API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
-    await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
