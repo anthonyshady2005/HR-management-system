346e262d2a41b7441ee7434344e791a51a640500
Eyad3skr <eyad.abdulaal@student.giu-uni.de>
2025-12-19 21:45:39 +0200

    fixing onboarding integration

--- frontend/src/app/employee/[id]/page.tsx
+++ frontend/src/app/employee/[id]/page.tsx
@@ -1086,6 +1086,7 @@ export default function EmployeeDetailPage() {
             </AlertDialogFooter>
           </AlertDialogContent>
         </AlertDialog>
+          </div>
         )}
       </div>
     </ProtectedRoute>
